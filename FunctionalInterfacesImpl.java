91b0ae17c945da62e6cc4e9894475226712e3991
Gaurav <gaurav13bisht@gmail.com>
2024-11-23 00:27:25 +0530

    Lambda Expressions and Function interfaces.

---
+++ FunctionalInterfacesImpl.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.function.*;
+
+public class FunctionalInterfacesImpl {
+    public static void main(String[] args) {
+
+        // Giving definition using Lambda expression, also we can create a class implementing this Predicate interface and give the definition for the method
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+        System.out.println(isEven.test(6));
+
+        Function<Integer, Integer> timesThree = x -> x * 3;
+        System.out.println(timesThree.apply(4));
+
+        Consumer<Integer> printElement = x -> System.out.println(x);
+        printElement.accept(34);
+
+        Supplier<Integer> giveRandomValue = () -> (int) (Math.random() * 100) % 16;
+        System.out.println(giveRandomValue.get());
+
+        BiPredicate<Integer, Integer> sumEven = (x, y) -> (x + y) % 2 == 0;
+        System.out.println(sumEven.test(2, 6));
+
+        BiFunction<Integer, Integer, Integer> multiply = (x, y) -> x * y;
+        System.out.println(multiply.apply(3, 6));
+
+        BiConsumer<Integer, Integer> printSum = (x, y) -> System.out.println(x + y);
+        printSum.accept(45, 5);
+
+    }
+}
---
+++ LambdaExpImpl.java
@@ -0,0 +1,42 @@
+interface AddNumbers{
+    public int calculate(int a, int b);
+}
+
+public class LambdaExpImpl {
+    public static void main(String[] args) {
+        // Lambda expression is used to write anonymous functions
+        // It doesn't have any access modifier, return type or name
+
+        // 1. Using Anonymous class for anonymous function
+        AddNumbers obj = new AddNumbers() {
+            @Override
+            public int calculate(int a, int b) {
+                return a+ b;
+            }
+        };
+
+        System.out.println(obj.calculate(1, 2));
+
+        // 2. Using Lambda expression for anonymous function
+
+//        A valid function definition for calculate would be:
+//        public int calculate(int a, int b){
+//            return a + b;
+//        }
+//        To convert it to Lambda exp, just remove access modifier, return type and name of the function and add -> sign:
+//        (int a, int b) -> {
+//            return a + b;
+//        }
+//        This will be your lambda expression
+
+        AddNumbers obj2 = (int a, int b) -> {
+                                return a + b;
+                            };
+
+        // More shorter versions:
+//        AddNumbers obj2 = (a, b) -> a + b;
+//        AddNumbers obj2 = Integer::sum;   // This is called Method reference
+
+        System.out.println(obj2.calculate(1, 2));
+    }
+}
