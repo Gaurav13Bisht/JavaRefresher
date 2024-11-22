967f766435c244084fddd63c3888dccc9ecfdf5c
Gaurav <gaurav13bisht@gmail.com>
2024-11-23 00:25:45 +0530

    toString(), equals(), hashCode()

---
+++ toStringEqualsHashcode.java
@@ -0,0 +1,20 @@
+public class toStringEqualsHashcode {
+
+    public static void main(String... args) {
+        Student student = new Student();
+
+        student.setAge(24);
+        student.setName("Gaurav");
+
+        // toString() check
+        System.out.println(student);
+
+        Student student2 = new Student();
+        student2.setAge(24);
+        student2.setName("Gaurav");
+
+        System.out.println(student.equals(student2));
+
+
+    }
+}
