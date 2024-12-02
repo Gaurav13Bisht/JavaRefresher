3e1895766351051bb6f0448ca3d19d914682bc48
Gaurav <gaurav13bisht@gmail.com>
2024-12-02 22:29:39 +0530

    MultiThreading 11/N (Future and Callable - Executor Framework)

--- multi/threading/FutureImpl.java
+++ multi/threading/FutureImpl.java
@@ -1,7 +1,6 @@
 package multi.threading;
 
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.*;
 
@@ -19,14 +18,18 @@ public class FutureImpl {
             System.out.println("Returning 24 value !");
         });
 
+        System.out.println(future.isDone());        // This will check if future is executed or not
+        System.out.println(future.isCancelled());        // This will check if future is cancelled or not
+        future.cancel(true);  // To cancel a future
+
         try {
             Thread.sleep(1000);
-            System.out.println(future.get());        // Execution will wait at this statement to get the result for the future variable first
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (ExecutionException e) {
+//            System.out.println(future.get());        // Execution will wait at this statement to get the result for the future variable first
+            future.get(2, TimeUnit.SECONDS);    // This will wait for 2 seconds to get the result, if task is not executed till that point, it will throw Timeout exception
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
             throw new RuntimeException(e);
         }
+
         System.out.println(56);
 //        executor.shutdown();
 
@@ -53,6 +56,16 @@ public class FutureImpl {
             for(Future<Integer> future3 : listFutures){
                 System.out.println(future3.get());
             }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("-------------------------------------------------------");
+        try {
+            Integer i = executor.invokeAny(list);           // This will invoke all the tasks mentioned in the list but will return only the 1st completed task and cancels all remaining tasks
+            System.out.println(i);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } catch (ExecutionException e) {
