05b05019f6a19ed0505184d293c145048d0ea1cd
Gaurav <gaurav13bisht@gmail.com>
2025-12-24 20:54:29 +0530

    MultiThreading 18/N

--- multi/threading/DeadLockImpl.java
+++ multi/threading/DeadLockImpl.java
@@ -32,31 +32,30 @@ public class DeadLockImpl {
 
 class PersonA {
     private final Lock lock = new ReentrantLock();
-
     // Either we can put lock.lock() at start and end of method or just put "synchronised" keyword at both methods
 
     public void transfer(PersonB personB){
-        // This will cause DEADLOCK since both are waiting for the respective class's lock to be released.
-//        lock.lock();
-
+//        lock.lock();            // Gives deadlock
         // Now this thread can only acquire this lock when personB's lock is free to be acquired
         // So at first, since both threads are started simultaneously, personB's thread lock would already be acquired so
         // the current thread will not acquire the current block's lock and wait.
         // Now in method of personB, since this block's lock is not acquired, it will acquire this lock and execute the code and
         // then release both the locks (both classes) which will be then freed for this class's thread to acquire
         // Thus DEADLOCK will be solved
-        synchronized (personB) {
+        synchronized (personB) {   // This will wait for personB lock to acquire and not lock PersonA lock thus solving deadlock
             System.out.println("Transferring to personB");
             personB.getAccountNumber();
             System.out.println("Transferred to personB");
         }
-//        lock.unlock();
+//        lock.unlock();    // Gives deadlock
     }
 
     public Integer getAccountNumber(){
-        lock.lock();
-        System.out.println("Giving acct number to personB");
-        lock.unlock();
+//        lock.lock();  // Gives deadlock
+        synchronized (this) {
+            System.out.println("Giving acct number to personB");
+        }
+//        lock.unlock();    // Gives deadlock
         return 12345;
     }
 }
@@ -65,22 +64,22 @@ class PersonB{
 
     private final Lock lock = new ReentrantLock();
 
-    // Either we can put lock.lock() at start and end of method or just put "synchronised" keyword at both methods
-
     public void transfer(PersonA personA){
-//        lock.lock();
-        synchronized (personA) {
+//        lock.lock();   // Gives deadlock
+        synchronized (personA) {    // This will wait for personA lock to acquire and not lock PersonB lock thus solving deadlock
             System.out.println("Transferring to personA");
             personA.getAccountNumber();
             System.out.println("Transferred to personA");
         }
-//        lock.unlock();
+//        lock.unlock();     // Gives deadlock
     }
 
     public Integer getAccountNumber(){
-        lock.lock();
-        System.out.println("Giving acct number to personA");
-        lock.unlock();
+//        lock.lock();   // Gives deadlock
+        synchronized (this) {
+            System.out.println("Giving acct number to personA");
+        }
+//        lock.unlock();     // Gives deadlock
         return 12345;
     }
 }
